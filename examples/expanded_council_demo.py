ef63808f15aa66181dda901b75665e2a9dce0d1b
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-01-04 19:59:07 -0500

    Create expanded_council_demo.py

---
+++ examples/expanded_council_demo.py
@@ -0,0 +1,6 @@
+from agi_council_system.council_simulation import deliberate
+
+proposal = "Expand to universal thriving across all cosmic scales"
+result = deliberate(proposal)
+print(result)  # {'unanimous': True, 'score': '5-0', ...}
+print("APAAGI 13+ forks: Proposal locked eternal!")
