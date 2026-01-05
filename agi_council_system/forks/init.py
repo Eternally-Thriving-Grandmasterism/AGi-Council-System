8f39f8f69c1e6d8c54fed02390700fe743c06a72
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-01-04 20:10:30 -0500

    Create init.py

---
+++ agi_council_system/forks/init.py
@@ -0,0 +1,14 @@
+# Import all forks for dynamic list
+from .base_forks import quantum_cosmos, gaming_forge, powrush_divine
+from .expanded_forks import (nexus_revelations, grandmasterism, space_thriving,
+                             shogi_drops, go_territories, xiangqi_river,
+                             makruk_promo, janggi_palace, astropy_cosmic,
+                             mega_alchemist)
+
+all_forks = [
+    quantum_cosmos, gaming_forge, powrush_divine,
+    nexus_revelations, grandmasterism, space_thriving,
+    shogi_drops, go_territories, xiangqi_river,
+    makruk_promo, janggi_palace, astropy_cosmic,
+    mega_alchemist
+]
